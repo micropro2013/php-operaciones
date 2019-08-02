2fc4fb6088b191c334ce43f39b807a5c3e419192
Your Name <you@example.com>
2019-08-02 11:45:33 -0600

    micropro - los archivos controlador y vista han sido creados

---
+++ vista.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        
+        
+    <center><h1>Operaciones en php</h1></center>
+       <?php 
+       
+       
+       ?>
+        
+        
+        
+    </body>
+</html>
+
