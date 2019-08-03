351c9d3a1a617a144159bddd2da7c9331399b76d
Your Name <you@example.com>
2019-08-03 11:04:15 -0600

    micropro2013 - la operacion de suma fue agregada satisfactoriamente

--- vista.php
+++ vista.php
@@ -8,8 +8,10 @@
         
     <center><h1>Operaciones en php</h1></center>
        <?php 
-       
-       
+       require("controlador.php");      
+       $insta = new controlador();
+       echo("<h2>suma de 3 + 4 </h2>");
+       echo("<h2>====>>>es igual al".$insta->suma(3, 4)."</h2><br><br>");
        ?>
         
         
