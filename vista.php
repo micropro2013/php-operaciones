be423cb3b3edd06de48097ad85080ea698d6af45
Your Name <you@example.com>
2019-08-04 09:48:00 -0600

    micropro - presentacion de la vista arreglada

--- vista.php
+++ vista.php
@@ -16,7 +16,7 @@
        echo("<h2>resta de 10 - 4 </h2>");
        echo("<h2>====>>>es igual a: -> ".$insta->resta(10, 4)."</h2><br><br>");
        
-       echo("<h2>resta de 15 * 4 </h2>");
+       echo("<h2>multiplicacion de 15 * 4 </h2>");
        echo("<h2>====>>>es igual a: -> ".$insta->multiplicacion(15, 4)."</h2><br><br>");
        ?>
         
