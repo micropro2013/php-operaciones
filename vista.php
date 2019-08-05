d27109b1ea5da43063addd744b27f3232cb1e7bb
Your Name <you@example.com>
2019-08-05 09:53:02 -0600

    micropro2013 arreglado en signo * que aparecia en vez de /

--- vista.php
+++ vista.php
@@ -19,7 +19,7 @@
        echo("<h2>multiplicacion de 15 * 4 </h2>");
        echo("<h2>====>>>es igual a: -> ".$insta->multiplicacion(15, 4)."</h2><br><br>");
        
-       echo("<h2>division de 15 * 3 </h2>");
+       echo("<h2>division de 15 / 3 </h2>");
        echo("<h2>====>>>es igual a: -> ".$insta->division(15, 3)."</h2><br><br>");
        ?>
         
